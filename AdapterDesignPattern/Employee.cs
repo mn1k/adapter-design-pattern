7702345ce60e65f9e7678f1ceec8ae432f36033d
Niharika Malvia <132000973+mn1k@users.noreply.github.com>
2023-10-03 16:17:48 +0530

    Add files via upload

---
+++ AdapterDesignPattern/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace AdapterDesignPattern
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public int Salary { get; set; }
+        public Employee(int id, string name, string designation, int salary)
+        {
+            ID = id;
+            Name = name;
+            Designation = designation;
+            Salary = salary;
+        }
+
+        public override bool Equals( object obj )
+        {
+            if ( obj == null ) { return false; }
+            Employee other = obj as Employee;
+
+            return (ID ==  other.ID && Name == other.Name && Designation == other.Designation && Salary == other.Salary);
+        }
+    }
+}
---
+++ AdapterDesignPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AdapterDesignPattern
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            // Storing the employees data in a string array
+            string[,] employeesArray = new string[6, 4]
+            {
+                {"1","Chandler","SE","10000"},
+                {"2","Monica","SE","20000"},
+                {"3","Rachel","SSE","30000"},
+                {"4","Joey","SE","40000"},
+                {"5","Ross","SSE","50000"},
+                {"6", "Pheobe", "SSE", "60000"}
+            };
+
+            Console.WriteLine("Passing Employee string array to Adapter\n");
+
+            IConvert convert = new EmployeeAdapter();
+            convert.ProcessEmployeeSalary(employeesArray);
+
+            Console.Read();
+        }
+    }
+}
